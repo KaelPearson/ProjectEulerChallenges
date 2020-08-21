3a675a1472e55e83455742ebff0c0897a1039e2c
Kael Pearson <kael.pearson@gmail.com>
2020-08-20 20:50:23 -0700

    Started problem28

---
+++ problem28.py
@@ -0,0 +1,16 @@
+'''
+    https://projecteuler.net/problem=27
+'''
+import time
+
+
+def main():
+    startTime = time.time()
+    
+    print()
+
+    endTime = time.time()
+    print(endTime - startTime, " Seconds taken")
+
+if(__name__ == "__main__"):
+    main()
