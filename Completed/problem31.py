5f637bfa69369fccba106f2a1a69cb0b2923797e
Kael Pearson <kael.pearson@gmail.com>
2020-08-22 15:03:27 -0700

    moved values and added comment

--- Completed/problem31.py
+++ Completed/problem31.py
@@ -3,15 +3,16 @@
 '''
 import time
 import math
-valuesToCheck = [1,2,5,10,20,50,100,200]
 def count(n):  
     table = [0 for i in range(n+1)] 
     table[0] = 1
+    valuesToCheck = [1,2,5,10,20,50,100,200]
     for i in valuesToCheck:
         table = addValue(i,n,table)
     return table[n]
 def addValue(num,n,table):
     for i in range(num, n+1):
+        # Check pos then add all values from previous
         table[i] += table[i-num]
     return table
 def main():
